aa0f3d4b693ff7bdd0c1774d1bd8bafd56be8fc7
kleberaluizio <kleberaluizio@gmail.com>
2024-10-14 21:30:56 -0300

    Entendendo o uso do Assertions.assertAll

--- junit-curso/src/test/java/barriga/domain/UserTest.java
+++ junit-curso/src/test/java/barriga/domain/UserTest.java
@@ -17,9 +17,26 @@ class UserTest {
         String password = "password";
         User user = new User(null, name, email, password);
 
-        assertEquals(name, user.getName());
-        assertEquals(email, user.getEmail());
+        assertEquals("John", user.getName());
+        assertEquals("john@john.coma", user.getEmail());
         assertEquals(password, user.getPassword());
 
     }
+
+    @Test
+    @DisplayName("Refatora o teste anterior de modo a garantir que todos os assertions sejam executados")
+    public void shouldCreateValidUserWhenDataIsValid_ASSERT_ALL() {
+        String name = "John John";
+        String email = "john@john.com";
+        String password = "password";
+        User user = new User(null, name, email, password);
+
+        Assertions.assertAll("User",
+            ()-> assertEquals("John", user.getName()),
+            ()-> assertEquals("john@john.com", user.getEmail()),
+            ()-> assertEquals("passworda", user.getPassword())
+        );
+    }
+
+
 }
