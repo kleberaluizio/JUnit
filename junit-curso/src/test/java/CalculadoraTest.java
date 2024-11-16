7dc746b2fd5bdf3e89f58178d486af1251a90704
kleberaluizio <kleberaluizio@gmail.com>
2024-11-16 17:48:51 -0300

    Utilizando as anotações ParameterizedTest e CsvSource, entendendo melhor o nullValues

--- junit-curso/src/test/java/CalculadoraTest.java
+++ junit-curso/src/test/java/CalculadoraTest.java
@@ -4,6 +4,9 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CalculadoraTest
 {
@@ -118,4 +121,28 @@ class CalculadoraTest
 		Assertions.assertNotNull(s1);
 		Assertions.assertThrows(NullPointerException.class, ()-> s3.equals(s1));
 	}
+
+	@DisplayName("Reviewing parameterized use cases")
+	@ParameterizedTest
+	@ValueSource(strings = {"Test1", "Test2"})
+	public void testStrings(String param) {
+		System.out.println(param);
+		Assertions.assertNotNull(param);
+	}
+
+	@DisplayName("Should Divide Correctly When ...")
+	@ParameterizedTest(name ="{3}")
+	@CsvSource(value = {
+			"10, 2, 5, ... positive values input",
+			"10, -2, -5, ... negative values input",
+			"10, 3, 3.333f, ... float result expected"})
+	void shouldDivideCorrectly(int numerator, int denominator, float EXPECTED_RESULT, String displayName) {
+		// Arrange & Act
+		float ACTUAL_RESULT = calculadora.dividir(numerator, denominator);
+
+		// Assert
+		Assertions.assertEquals(EXPECTED_RESULT, ACTUAL_RESULT, 0.001);
+
+	}
+
 }
--- junit-curso/src/test/java/barriga/domain/UserTest.java
+++ junit-curso/src/test/java/barriga/domain/UserTest.java
@@ -5,6 +5,8 @@ import barriga.exceptions.ValidationException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -55,5 +57,22 @@ class UserTest {
     public void shouldRejectUserWhenPasswordIsNotProvided() {
         ValidationException exception = assertThrows(ValidationException.class,  ()-> UserBuilder.aUser().withPassword(null).now());
         Assertions.assertEquals("Password cannot be null or empty", exception.getMessage());
+    }
+
+    @DisplayName("Should Reject User When")
+    @ParameterizedTest(name = "{4}")
+    @CsvSource(value = {
+            "NULL, josef@email.com, password, Name cannot be null or empty, ... name is null or empty",
+            "Josef, N/A, password, E-mail cannot be null or empty, ... e-mail is null or empty",
+            "Josef, josef@email.com, undefined, Password cannot be null or empty, ... password is null or empty",},
+            nullValues = {"NULL", "N/A", "undefined"})
+    void shouldRejectUserWhen(String name, String email, String password, String expectedExceptionMessage, String displayMessage) {
+        ValidationException exception = assertThrows(ValidationException.class, () ->
+                {
+                    UserBuilder.aUser().withName(name).withEmail(email).withPassword(password).now();
+                }
+        );
+        Assertions.assertEquals(expectedExceptionMessage, exception.getMessage());
+
     }
 }
